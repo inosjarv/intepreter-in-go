db9a4330dc51c5b5121eb952e66348f26a1dd84a
Vraj Soni <vrajds8725@gmail.com>
2024-11-07 10:12:41 +0530

    Chapter 1 Completed

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/inosjarv/interpreter-in-go
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/inosjarv/interpreter-in-go/repl"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is Monkey Programming Languages\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
